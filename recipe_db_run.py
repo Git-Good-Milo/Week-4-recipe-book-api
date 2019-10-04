23a9e3eb6e39f019565a8a72f113470bb9f42208
Miles Eastwood <meastwood@spartaglobal.com>
2019-10-04 14:26:50 +0100

    Completed 3/4 user inputs. Still needd to complete the option to add to a .txt file

--- recipe_db_run.py
+++ recipe_db_run.py
@@ -14,7 +14,7 @@ connect_to_recipe_db = ConnectMsS('Passw0rd2018')
 recipe1 = Recipe("Milo Spag Bowel", "Spagetti, beef mince, canned tomatoes, beef stock, onions, garlic, thyme, rosmery", "N85 9LN")
 recipe2 = Recipe("tasty Cookies", "chocolate, milk, eggs, flour, butter", "N169LN")
 recipe3 = Recipe("American Pancakes", "Freedom", "90210")
-user_recipe = Recipe(f"'{recipe_name}', '{ingredients}', '{postcode}'")
+#user_recipe = Recipe(f"'{recipe_name}', '{ingredients}', '{postcode}'")
 
 
 # Use methods to add a recipe to the database
@@ -39,7 +39,7 @@ user_recipe = Recipe(f"'{recipe_name}', '{ingredients}', '{postcode}'")
 
 # Use method to pull more postcode information from API
 
- info_recipe2 = get_post_code(recipe2.post_code)
+#info_recipe2 = get_post_code(recipe2.post_code)
 # print(info_recipe2)
 
 
@@ -53,10 +53,51 @@ option_2 = print("Would you like to view all recipes or just one?")
 option_3 = print("Would you like to delete a shoddy recipe from our database?")
 option_4 = print("Would you like to export a recipe to a .txt file?")
 user_input = input("Enter your choice:  ")
+
 while True:
     if user_input == "1":
-        get_recipe = input("Please enter your recipe name: ")
-        get_ingridiant = input("Please enter your ingreiants: ")
-        get_postCode = input("Please enter your postcode: ")
-        connect_to_recipe_db.add_recipe_to_db(get_recipe,get_ingridiant,get_postCode)
+        get_recipe = input("'Please enter your recipe name: '")
+        get_ingridiant = input("'Please enter your ingreiants: '")
+        get_postCode = input("'Please enter your postcode: '")
+        user_recipe = connect_to_recipe_db.add_recipe_to_db(get_recipe,get_ingridiant,get_postCode)
+        print(f"{user_recipe.recipe_name} has been added to the database :)")
+
+    elif user_input == "2":
+        view_recipe_s = input("Would you like to view one recipe or all of them?")
+        if view_recipe_s == "1":
+            view_by_title = connect_to_recipe_db.title_search
+        elif view_recipe_s == "all":
+            view_all = connect_to_recipe_db.read_all_recipes()
+        else:
+            print("Sorry, I didnt quite get that")
+            break
+
+    elif user_input == "3":
+        print("Which recipe would you like to delete?")
+        delete_recipe = connect_to_recipe_db.delete_recipe_from_db
+        print("That recipe has been deleted")
+
+    # elif user_input == "4":
+    #     input_recipe_for_txt = input("Which recipe would you like to export?")
+    #
+    #     if input_recipe_for_txt == user_recipe.recipe_name:
+    #         recipe_to_export = user_recipe.append_a_recipe_to_file()
+    #
+    #     elif input_recipe_for_txt == "I'd like to choose a recipe":
+    #         recipe_to_choose = input("Which recipe will it be then?")
+    #
+    #         if recipe_to_choose ==
+
+    elif user_input == "exit":
+        break
+
+
+
+
+
+
+
+
+
+
 
